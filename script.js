32ba714ee94115c8f86fc20e8eaf9a29e63211bc
06091210 <firp771@gmail.com>
2025-06-23 19:04:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,143 @@
 const quizData = [
   {
-    question: "",
+    question: "次の単語や文章を英訳せよ　2度",
     choices: [
-      { text: "" },
-      { text: "" },
-      { text: "" },
-      { text: "" },
+      { text: "twice" },
+      { text: "once" },
+      { text: "two times" },
+      { text: "second" },
     ],
-    correct: ,
+    correct: 0,
+  },
+  {
+    question: "未確認飛行物体",
+    choices: [
+      { text: "UFO" },
+      { text: "UFOW" },
+      { text: "U.F.O." },
+      { text: "U.F.O" },
+    ],
+    correct: 2,
+  },
+  {
+    question: "直面する",
+    choices: [
+      { text: "get" },
+      { text: "fase" },
+      { text: "meet" },
+      { text: "face" },
+    ],
+    correct: 3,
+  },
+  {
+    question: "投稿する",
+    choices: [
+      { text: "pond" },
+      { text: "spend" },
+      { text: "post" },
+      { text: "update" },
+    ],
+    correct: 2,
+  },
+  {
+    question: "マスメディア",
+    choices: [
+      { text: "media" },
+      { text: "medea" },
+      { text: "mathmadia" },
+      { text: "masmedea" },
+    ],
+    correct: 0,
+  },
+  {
+    question: "ソーシャルメディア",
+    choices: [
+      { text: "media" },
+      { text: "social media" },
+      { text: "socale midea" },
+      { text: "midea" },
+    ],
+    correct: 1,
+  },
+  {
+    question: "ブログ",
+    choices: [
+      { text: "brog" },
+      { text: "vlag" },
+      { text: "vrog" },
+      { text: "blog" },
+    ],
+    correct: 3,
+  },
+  {
+    question: "〜を思いつく",
+    choices: [
+      { text: "get up to~" },
+      { text: "come up to~" },
+      { text: "come up with~" },
+      { text: "get come with~" },
+    ],
+    correct: 2,
+  },
+  {
+    question: "研究会",
+    choices: [
+      { text: "scienction" },
+      { text: "solution" },
+      { text: "workshop" },
+      { text: "warcshap" },
+    ],
+    correct: 2,
+  },
+  {
+    question: "魅了する",
+    choices: [
+      { text: "attract" },
+      { text: "atruct" },
+      { text: "attruct" },
+      { text: "atract" },
+    ],
+    correct: 0,
+  },
+  {
+    question: "ビデオクリップ",
+    choices: [
+      { text: "a video crips" },
+      { text: "video clips" },
+      { text: "video crips" },
+      { text: "a vedio crip" },
+    ],
+    correct: 1,
+  },
+  {
+    question: "編集する",
+    choices: [
+      { text: "make" },
+      { text: "editation" },
+      { text: "edit" },
+      { text: "rode" },
+    ],
+    correct: 2,
+  },
+  {
+    question: "私は以前にも同じような問題に直面したことがある．",
+    choices: [
+      { text: "I have been facing the plobrem similar later." },
+      { text: "I've been faced same problem." },
+      { text: "I have been faced same problems before." },
+      { text: "I've faced the same problem before." },
+    ],
+    correct: 3,
+  },
+  {
+    question: "彼は今までで3回マラソンをしたことがある．",
+    choices: [
+      { text: "He have running a marason three times." },
+      { text: "He's run a marathon three times." },
+      { text: "He has ran a marathonn three times." },
+      { text: "He has running a marathon three times." },
+    ],
+    correct: 1,
   },
 ];
 let currentQuiz = quizData;
